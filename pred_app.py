1bb8e7a0b1a40a0c33e0aecfda22c57d40d0e53d
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2023-09-08 10:15:15 +0700

    Create pred_app.py

---
+++ pred_app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+from pycaret.classification import *
+from pycaret.regression import *
+
+# Title
+st.title("PyCaret Prediction App")
+
+# Upload CSV file
+st.sidebar.header("Upload Data")
+uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+
+# Choose task (classification or regression)
+task = st.sidebar.selectbox("Select a Task", ("Classification", "Regression"))
+
+# Load data and select appropriate PyCaret function
+if uploaded_file is not None:
+    @st.cache
+    def load_data():
+        data = pd.read_csv(uploaded_file)
+        return data
+
+    data = load_data()
+
+    if task == "Classification":
+        st.subheader("Classification Task")
+
+        # Add code for classification tasks using PyCaret here
+        # Example:
+        if st.button("Run Classification"):
+            setup(data, target="target_column")
+            compare_models()
+
+    elif task == "Regression":
+        st.subheader("Regression Task")
+
+        # Add code for regression tasks using PyCaret here
+        # Example:
+        if st.button("Run Regression"):
+            setup(data, target="target_column")
+            compare_models()
+
+    st.sidebar.markdown("### Data Sample")
+    st.sidebar.write(data.head())
+
+else:
+    st.warning("Please upload a CSV file.")
+
+# Optional: Add more Streamlit components for user interaction and displaying results.
